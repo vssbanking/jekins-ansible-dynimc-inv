b5205f14040a4cf9c044e5356029818d1d0db16f
vineethreddydesireddy <127300845+vineethreddydesireddy@users.noreply.github.com>
2023-05-06 14:47:55 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "AWSInstance"{
   ami = "ami-097bd6037de54b1dc"
   instance_type = "t2.micro"
   key_name = "devops"
-  security_groups = ["lauch-wizard-3"]
+  security_groups = ["launch-wizard-3"]
   tags = {
   Name = "tomservers"
   } 
